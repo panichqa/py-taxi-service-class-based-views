678ca5656d83f90e7fce98ebc4ff2697bf2602c5
Vlada Bilai <vlada.belayy@gmail.com>
2024-09-09 21:43:29 +0300

    my page fixed flake8 again

--- taxi/urls.py
+++ taxi/urls.py
@@ -11,11 +11,10 @@ from .views import (
 
 urlpatterns = [
     path("", index, name="index"),
-    path(
-        "manufacturers/",
+    path("manufacturers/",
          ManufacturerListView.as_view(),
          name="manufacturer-list"
-    ),
+         ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path(
         "cars/<int:pk>/",
@@ -27,11 +26,10 @@ urlpatterns = [
         DriverListView.as_view(),
         name="driver-list"
     ),
-    path(
-        "drivers/<int:pk>/",
+    path("drivers/<int:pk>/",
          DriverDetailView.as_view(),
          name="driver-detail"
-    ),
+         ),
 ]
 
 app_name = "taxi"
