3878d520369506e8de4bea2190c822f018f7ef5e
Vlada Bilai <vlada.belayy@gmail.com>
2024-09-09 22:00:13 +0300

    try

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-8ovil3xu6=eaoqd#-#&ricv159poh5_lgm*)-dfcjqe=yc"
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
